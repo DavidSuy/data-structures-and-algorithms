4663ea8fd953efad8f8db148305ecb8b1869dfb2
David Suy <DavidSuyDev@gmail.com>
2022-10-24 23:52:21 -0700

    insertion sort

---
+++ sorting/insertion/index.js
@@ -0,0 +1,16 @@
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let j = i - 1;
+    let temp = arr[i];
+
+    while (j >= 0 && temp < arr[j]) {
+      arr[j + 1] = arr[i];
+      j = j - 1;
+    }
+    arr[j + 1] = temp;
+  }
+  return arr;
+}
+
+let x = insertionSort([2, 1, 9, 76, 4]);
+console.log(x);
