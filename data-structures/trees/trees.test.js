e6e6eeed70a0982f6f99bb693af4e9efc7df2214
David Suy <DavidSuyDev@gmail.com>
2022-12-08 18:33:19 -0500

    add breadth first test

--- data-structures/trees/trees.test.js
+++ data-structures/trees/trees.test.js
@@ -40,4 +40,17 @@ describe("Binary Search Trees", () => {
     expect(bst.contains(10)).toBe(false);
     expect(bst.contains(3)).toBe(true);
   });
+  it("Should return [2,7,5,2,6,9,5,11,4], given a certain tree", () => {
+    let tree = new BinarySearchTree();
+    tree.root = new Node(2);
+    tree.root.left = new Node(7);
+    tree.root.right = new Node(5);
+    tree.root.left.left = new Node(2);
+    tree.root.left.right = new Node(6);
+    tree.root.left.right.left = new Node(5);
+    tree.root.left.right.right = new Node(11);
+    tree.root.right.right = new Node(9);
+    tree.root.right.right.left = new Node(4);
+    expect(tree.BFS()).toEqual([2, 7, 5, 2, 6, 9, 5, 11, 4]);
+  });
 });
