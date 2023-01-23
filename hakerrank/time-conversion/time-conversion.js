84157be23038df6ec17cfdf6cffae6424a46720a
David Suy <DavidSuyDev@gmail.com>
2023-01-22 23:53:26 -0500

    finish time conversion function

--- hakerrank/time-conversion/time-conversion.js
+++ hakerrank/time-conversion/time-conversion.js
@@ -2,26 +2,22 @@ function timeConversion(s) {
   // separate the string by : and store it in a variable called timeArr
   let timeArr = s.split(":");
 
-  console.log(timeArr[2].substr(2, 4));
-
   // check if time is AM or PM
   if (timeArr[2].substr(2, 4) === "PM") {
-    console.log("we in it");
     // if so add 12 to timeArr[0]
-    timeArr[0] + 12;
+    timeArr[0] = Number(timeArr[0]) + 12;
   }
 
   // Only the first two chars in arr[2]
   // console.log(typeof timeArr[2]);
   timeArr[2] = timeArr[2].substr(0, 2);
 
-  // timeArr[2] = timeArr[2].split(0, 2);
-  console.log(timeArr);
   // join timeArr
   // return timeArr
+  return timeArr.join(":");
 }
 
 let testTime = "07:05:45PM";
 // let testTime = "14:22:13PM";
 
-timeConversion(testTime);
+console.log(timeConversion(testTime));
