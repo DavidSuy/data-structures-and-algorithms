b91f8298c210a767abac37562dd0500108a419e7
David Suy <DavidSuyDev@gmail.com>
2023-01-26 02:22:57 -0500

    update time-conversion test

--- hakerrank/time-conversion/time-conversion.test.js
+++ hakerrank/time-conversion/time-conversion.test.js
@@ -2,9 +2,9 @@ const timeConversion = require("./time-conversion");
 
 describe("timeConversion test", () => {
   it("Should return 19:05:45, when given 07:05:45PM", () => {
-    timeConversion("07:05:45PM");
+    expect(timeConversion("07:05:45PM")).toBe("19:05:45");
   });
   it("Should return 00:40:22 when given 12:40:22AM", () => {
-    timeConversion("12:40:22AM");
+    expect(timeConversion("12:40:22AM")).toBe("00:40:22");
   });
 });
